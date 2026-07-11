953a74f7e5cccf04c788dc67212fdbdd9c3de0b8
Israel Omotayo <omotayoisrael24@gmail.com>
2026-07-11 18:08:00 +0100

    Updated security settings

--- vitely/settings/base.py
+++ vitely/settings/base.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     # It intercepts /static/ requests before they ever hit Django's routing,
     # so WSGI workers are never tied up serving JS/CSS files.
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "core.middleware.SecurityHeadersMiddleware",
     'core.middleware.EnforceCustomDomainMiddleware', # for custom domain
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
